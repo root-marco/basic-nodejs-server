f4fd43d6b2150c90e288f3f5b406f35326c4abf2
chaos <rootslowed@gmail.com>
2021-02-04 18:13:48 -0300

    read html file with error handling and send to the user

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 import http from 'http';
+import fs from 'fs';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
 
-	res.write('Hello Node');
-	res.end();
+	res.writeHead(200, {
+		'Content-Type': 'text/html',
+	});
+
+	fs.readFile('index.html', (error, data) => {
+
+		if (error) {
+			res.writeHead(404);
+			res.write('Error: File not found');
+		} else {
+			res.write(data);
+		}
+		res.end();
+	});
 
 });
 
 server.listen(port, (error) => {
+	
 	if (error) {
 		console.log(`Something went wrong ${error}`);
 	} else {
 		console.log(`Server running on port ${port}`);
 	}
+	
 });
