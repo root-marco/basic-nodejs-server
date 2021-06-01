e9d3d643b8eb780329d66ee22308f7e6985eb010
slowed <rootslowed@gmail.com>
2021-06-01 14:35:27 -0300

    user router imported and set up

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+// ROUTES
+import userRouter from "./routes/user.js";
+app.use("/user", userRouter);
+
 app.listen(PORT, () => {
-	console.log("server running");
+  console.log("server running");
 });
