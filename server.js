63575777fc2f700a219d799746a139f0ccf824d8
slowed <rootslowed@gmail.com>
2021-06-02 13:52:00 -0300

    ejs set up, path and dotenv imported

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+// EJS
+app.set("view-engine", "ejs");
+app.set("views", path.join(`${process.cwd()}/src/views`));
+
 // ROUTES
-import userRouter from "./src/routes/userRouter.js";
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+import userRouter from 
+"./src/routes/userRouter.js";
 app.use("/user", userRouter);
 
 app.listen(PORT, () => {
-  console.log("server running");
+  console.log("server running" + PORT);
 });
