73d478bb2bd2d03d0a98ac2599a6daa0027fbbd7
slowed <rootslowed@gmail.com>
2021-06-03 11:46:52 -0300

    register new user controller done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,43 @@
-export async function getLogin (req, res) {
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+
+export async function getLogin(req, res) {
 
   res.render("login.ejs");
 
 }
 
-export async function getRegister (req, res) {
+export async function getRegister(req, res) {
 
   res.render("register.ejs");
+
+}
+
+export async function postLogin(req, res) {
+
+
+
+}
+
+export async function postRegister(req, res) {
+
+  const body = req.body;
+
+  if (!(body.name && body.email && body.password)) {
+    return res.status(400).send({
+      error: "data not formatted properly",
+    });
+  }
+
+  const newUser = new User(body);
+  newUser.password = await bcrypt.hash(newUser.password, 10);
+
+  try {
+    await newUser.save();
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    res.redirect("/user/register");
+  }
 
 }
