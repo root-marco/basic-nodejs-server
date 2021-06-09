359aa817f80a1713389a32710bba24d43af09fae
slowed <rootslowed@gmail.com>
2021-06-09 14:55:10 -0300

    user router done

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,13 +1,16 @@
 import express from "express";
 
-import * as userController from
-"../controllers/userController.js";
+import * as userController from "../controllers/userController.js";
+import checkNotAuthenticated from "../middlewares/checkNotAuthenticated.js";
 
 const router = express.Router();
 
-router.get("/login", userController.getLogin);
-router.get("/register", userController.getRegister);
-router.post("/login", userController.postLogin);
-router.post("/register", userController.postRegister);
+router.get("/login", checkNotAuthenticated, userController.getLogin);
+router.get("/register", checkNotAuthenticated, userController.getRegister);
+
+router.post("/login", checkNotAuthenticated, userController.postLogin);
+router.post("/register", checkNotAuthenticated, userController.postRegister);
+
+router.delete("/logout", userController.deleteLogout);
 
 export default router;
