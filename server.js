4b92511091ea1f96dd08e8e51d9e67672ae3bc71
slowed <rootslowed@gmail.com>
2021-06-09 14:55:36 -0300

    app done

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-import path from "path";
-import express from "express";
+import methodOverride from "method-override";
+import session from "express-session";
+import flash from "express-flash";
+import passport from "passport";
 import mongoose from "mongoose";
+import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+if (process.env.NODE_ENV !== "production") dotenv.config();
+import configAuth from "./src/middlewares/passport.js";
+import userRouter from "./src/routes/userRouter.js";
+import checkAuthenticated from "./src/middlewares/checkAuthenticated.js";
 
+// APP
 const app = express();
 const PORT = process.env.PORT;
 const MONGOURI = process.env.MONGOURI;
+const SECRET = process.env.SESSION;
 const DIR = process.cwd();
-app.use(express.urlencoded({
-  extended: false,
-}));
 
 // MONGOOSE
 try {
@@ -28,12 +34,29 @@ try {
 app.set("view-engine", "ejs");
 app.set("views", path.join(`${DIR}/src/views`));
 
-// ROUTES
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-import userRouter from "./src/routes/userRouter.js";
+// MIDDLEWARES
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({
+  extended: false,
+}));
+app.use(express.json());
+app.use(flash());
+configAuth(passport);
+app.use(session({
+  secret: SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use("/user", userRouter);
+
+// INDEX
+app.get("/", checkAuthenticated, async (req, res) => {
+  res.render("index.ejs", {
+    name: req.user.name,
+  });
+});
 
 // LISTEN
 app.listen(PORT, () => {
